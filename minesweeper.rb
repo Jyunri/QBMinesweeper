eaa2564d718a7289e93576337d9fff2ced5ec33d
Jimy <nekojimy@gmail.com>
2017-10-04 03:45:00 -0300

    major refactor

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,165 +1,213 @@
 require 'pry'
 require 'matrix'
 
+# Representation of a Game
 class Minesweep
-  attr_accessor :mine_field, :row, :col, :bombs_count, :clear_cell_count, :game_over
+  attr_accessor :mine_field, :row, :col, :bombs_count, :clear_cell_count,
+                :game_over
 
-  def initialize(row,col,bombs_count)
-    puts "Generating #{row}x#{col} Field.."
+  def initialize(row, col, bombs_count)
     @row = row
     @col = col
     @bombs_count = bombs_count
-    @clear_cell_count = @row * @col  - @bombs_count
+    @clear_cell_count = @row * @col - @bombs_count
     @game_over = false
-    @mine_field = Matrix.build(row+2,col+2) { |r,c|
-      if((r == 0) || (c == 0) || (r == row + 1) || (c == col + 1))  # check if is a border cell
-        type = -1
-        status = 1
+    @mine_field = generate_field
+    set_bombs
+  end
+
+  def generate_field
+    borders = [0, row + 1, col + 1]
+    Matrix.build(row + 2, col + 2) do |r, c|
+      # check if is a border cell
+      if borders.include?(r) || borders.include?(c)
+        Cell.new(r, c, -1, 1)
       else
-        type = 0
-        status = 0
+        Cell.new(r, c, 0, 0)
       end
-      Cell.new(type,status)
-    }
-    set_bombs()
-    count_bombs_in_neighborhood()
+    end
   end
 
-  def set_bombs()
-    puts "Setting #{@bombs_count} bombs.."
-    (1..@bombs_count).each { |bomb|
-      bomb_row = 0
-      bomb_col = 0
+  def set_bombs
+    (1..@bombs_count).each do
+      random_row = random_col = 0
       loop do
-        bomb_row = rand(@row)+1
-        bomb_col = rand(@col)+1
-        break if @mine_field[bomb_row,bomb_col].isClear()
+        random_row = rand(@row) + 1
+        random_col = rand(@col) + 1
+        break if @mine_field[random_row, random_col].clear?
       end
-      @mine_field[bomb_row,bomb_col].type = 1
-    }
+      @mine_field[random_row, random_col].type = 1
+
+      # recalculate the bomb count per cell dynamically
+      update_bomb_count(random_row, random_col)
+    end
   end
 
-  def count_bombs_in_neighborhood()
-    puts "Counting bombs in neighborhood for each cell"
-    for i in 1..@row
-      for j in 1..@col
-        @mine_field[i,j].bombs_in_neighborhood += 1 if @mine_field[i-1,j-1].isBomb
-        @mine_field[i,j].bombs_in_neighborhood += 1 if @mine_field[i-1,j].isBomb
-        @mine_field[i,j].bombs_in_neighborhood += 1 if @mine_field[i-1,j+1].isBomb
-        @mine_field[i,j].bombs_in_neighborhood += 1 if @mine_field[i,j-1].isBomb
-        @mine_field[i,j].bombs_in_neighborhood += 1 if @mine_field[i,j+1].isBomb
-        @mine_field[i,j].bombs_in_neighborhood += 1 if @mine_field[i+1,j-1].isBomb
-        @mine_field[i,j].bombs_in_neighborhood += 1 if @mine_field[i+1,j].isBomb
-        @mine_field[i,j].bombs_in_neighborhood += 1 if @mine_field[i+1,j+1].isBomb
+  def update_bomb_count(bomb_row, bomb_col)
+    (bomb_row - 1..bomb_row + 1).each do |r|
+      (bomb_col - 1..bomb_col + 1).each do |c|
+        next if r == bomb_row && c == bomb_col
+        cell = @mine_field[r, c]
+        cell.bombs_in_neighborhood += 1 if cell.clear?
       end
     end
   end
 
   def victory?
-    return true if @clear_cell_count == 0
+    return true if @clear_cell_count.zero?
   end
 
   def still_playing?
-    (victory? || @game_over)? false : true
+    victory? || @game_over ? false : true
   end
 
-
-  # User inputs
-  def check_cell()
-    puts "Enter the row where you want check"
+  def game_check_cell
+    puts 'Enter the row where you want check'
     input_row = gets.to_i
-    puts "Enter the column where you want to check"
+    puts 'Enter the column where you want to check'
     input_col = gets.to_i
 
-    # TODO - raise as exception if invalid row/column
-    if(@mine_field[input_row,input_col].isBomb)
-      puts "Oh no it's a bomb!"
-      @game_over = true
-      return
-    elsif(@mine_field[input_row,input_col].isChecked)
-      puts "Hey! This position has already been checked!"
-    elsif(@mine_field[input_row,input_col].isUnchecked)
-      if(@mine_field[input_row,input_col].bombs_in_neighborhood == 0)
-        puts "Great! A expansion!"
-        expand_neighborhood(input_row,input_col)
+    # Passing the game class as parameter to handle neighbor cells
+    @mine_field[input_row, input_col].check_cell(self)
+
+    true
+  end
+
+  def game_set_flag
+    puts 'Enter the row where you want to set a flag'
+    input_row = gets.to_i
+    puts 'Enter the column where you want to set a flag'
+    input_col = gets.to_i
+
+    # TODO: - raise as exception if invalid row/column
+    @mine_field[input_row, input_col].set_flag
+  end
+
+  def board_state(opt = {})
+    puts opt[:xray]
+  end
+end
+
+# Representation of a Cell/Button.
+# A Cell can be a Bomb, a Border or a Clear Cell.
+# Furthermore, it can be Checked, Unchecked or Flagged
+class Cell
+  attr_accessor :row, :col, :type, :status, :hasFlag, :bombs_in_neighborhood
+
+  def initialize(row, col, type, status)
+    @row = row
+    @col = col
+    @type = type
+    @bombs_in_neighborhood = 0
+    @status = status
+  end
+
+  def check_cell(game)
+    if bomb?
+      game.game_over = true
+    elsif unchecked?
+      @status = 1
+      game.clear_cell_count -= 1
+      puts "Nice! There's still #{game.clear_cell_count} clear cells left!"
+      expand_neighborhood(game) if bombs_in_neighborhood.zero?
+    else
+      puts 'Invalid position!'
+    end
+  end
+
+  def expand_neighborhood(game)
+    (row - 1..row + 1).each do |r|
+      (col - 1..col + 1).each do |c|
+        next if r == row && c == col
+        cell = game.mine_field[r, c]
+        next if cell.checked?
+        cell.status = 1
+        game.clear_cell_count -= 1
+        cell.expand_neighborhood(game) if cell.expandable?
       end
-      @mine_field[input_row,input_col].status = 1
-      @clear_cell_count -= 1
-      puts "Nice! It's not a bomb! There's still #{@clear_cell_count} clear cells to be discovered!"
+    end
+  end
+
+  def set_flag
+    if flagged?
+      @status = 0
+      puts "Flag unset from position (#{@row},#{@col})"
+    elsif unchecked?
+      @status = 2
+      puts "Flag set on position (#{@row},#{@col})"
     else
-      puts "Invalid position!"
-      return false
-    end
-
-    puts
-    current_print()
-
-    puts
-
-    return true
-  end
-
-  # TODO optimize expansion
-  def expand_neighborhood(r,c)
-    neighbours = Matrix[[r-1,c-1],[r-1,c],[r-1,c+1],[r,c-1],[r,c+1],[r+1,c-1],[r+1,c],[r+1,c+1]]
-    for i in 0...neighbours.row_count
-      r_expand = neighbours[i,0]
-      c_expand = neighbours[i,1]
-      puts "expanding (#{r_expand},#{c_expand})"
-      check_cell_from_expand(r_expand,c_expand) if mine_field[r_expand,c_expand].isExpandable
+      puts 'Invalid position!'
+      false
     end
   end
 
-  def check_cell_from_expand(r,c)
-    @mine_field[r,c].status = 1
-    @clear_cell_count -= 1
-    expand_neighborhood(r,c)  # Recursive for the next expandables
+  # TODO: try to convert this in something like #define BORDER -1
+  # type conventions
+  def border?
+    return true if @type == -1
   end
 
+  def clear?
+    return true if @type.zero?
+  end
 
-  def set_flag()
-    puts "Enter the row where you want to set a flag"
-    input_row = gets.to_i
-    puts "Enter the column where you want to set a flag"
-    input_col = gets.to_i
+  def bomb?
+    return true if @type == 1
+  end
 
-    # TODO - raise as exception if invalid row/column
-    if(@mine_field[input_row,input_col].isFlagged)
-      @mine_field[input_row,input_col].status = 0
-      puts "Flag unsetted from position (#{input_row},#{input_col})"
-    elsif(@mine_field[input_row,input_col].isUnchecked)
-      @mine_field[input_row,input_col].status = 2
-      puts "Flag setted on position (#{input_row},#{input_col})"
+  # status conventions
+  def checked?
+    return true if @status == 1
+  end
+
+  def unchecked?
+    return true if @status.zero?
+  end
+
+  def flagged?
+    return true if @status == 2
+  end
+
+  def expandable?
+    return true if @bombs_in_neighborhood.zero? && clear?
+  end
+
+  def to_s
+    if border?
+      'x'
+    elsif bomb?
+      '#'
     else
-      puts "Invalid position!"
-      return false
+      bombs_in_neighborhood.to_s
     end
+  end
+end
 
-    puts
-    current_print()
-
-    puts
-    return true
+# Simple printer class. Just a non-friendly representation
+class Printer
+  attr_accessor :board_state
+  def initialize(board_state)
+    @board_state = board_state
   end
 
-  def xray_print()
-    puts "X-ray field:"
-    for i in 0..@row+1
-      for j in 0..@col+1
-        print "#{@mine_field[i,j]}\t"
+  def xray_print
+    puts 'X-ray field:'
+    (0..@board_state.row + 1).each do |i|
+      (0..@board_state.col + 1).each do |j|
+        print "#{@board_state.mine_field[i, j]}\t"
       end
       puts
     end
     puts
   end
 
-  def current_print()
-    puts "Current state of field:"
-    for i in 0..@row+1
-      for j in 0..@col+1
-        if(@mine_field[i,j].isChecked)
-          print "#{@mine_field[i,j]}\t"
-        elsif(@mine_field[i,j].isFlagged)
+  def current_print
+    puts 'Current state of field:'
+    (0..@board_state.row + 1).each do |i|
+      (0..@board_state.col + 1).each do |j|
+        if @board_state.mine_field[i, j].checked?
+          print "#{@board_state.mine_field[i, j]}\t"
+        elsif @board_state.mine_field[i, j].flagged?
           print "F\t"
         else
           print "?\t"
@@ -169,73 +217,26 @@ class Minesweep
     end
     puts
   end
-
 end
 
-class Cell
-  attr_accessor :type, :status, :hasFlag, :bombs_in_neighborhood
+def new_game_input
+  puts 'Enter with number of rows'
+  input_rows = gets.to_i
+  puts 'Enter with number of columns'
+  input_columns = gets.to_i
+  puts 'Enter with number of bombs'
+  input_bombs = gets.to_i
 
-  def initialize(type,status)
-    @type = type
-    @bombs_in_neighborhood = 0
-    @status = status
-  end
+  raise ArgumentError, 'Row is less than 0' unless input_rows > 0
+  raise ArgumentError, 'Column is less than 0' unless input_columns > 0
+  raise ArgumentError, 'Bombs is less than 0' unless input_bombs > 0
 
-  # TODO try to convert this in something like #define BORDER -1
-  # type conventions
-  def isBorder
-    return true if @type == -1
-  end
-
-  def isClear
-    return true if @type == 0
-  end
-
-  def isBomb
-    return true if @type == 1
-  end
-
-  # status conventions
-  def isChecked
-    return true if @status == 1
-  end
-
-  def isUnchecked
-    return true if @status == 0
-  end
-
-  def isFlagged
-    return true if @status == 2
-  end
-
-  def isExpandable
-    return true if isUnchecked && @bombs_in_neighborhood == 0 && isClear
-  end
-
-  def to_s
-    if(isBorder)
-      return "x"
-    elsif(isBomb)
-      return "#"
-    else
-      return "#{bombs_in_neighborhood}"
-    end
-  end
-
+  Minesweep.new(input_rows, input_columns, input_bombs)
 end
 
-#todo: create validations of rows, columns > 0 and bombs <= row*columns
-# puts "Enter with number of rows"
-# input_rows = gets.to_i
-# puts "Enter with number of columns"
-# input_columns = gets.to_i
-# puts "Enter with number of bombs"
-# input_bombs = gets.to_i
+g = new_game_input
 
-m = Minesweep.new(5,5,4)
-# m = Minesweep.new(input_rows,input_columns,input_bombs)
-
-m.current_print()
+Printer.new(g).current_print
 
 loop do
   menu = "Menu:\
@@ -245,34 +246,33 @@ loop do
     \nx: Print x-ray field\
     \ne: Exit programn\n"
   puts menu
-  print_mode = gets.chomp()
+  print_mode = gets.chomp
   puts
   case print_mode
-  when "c"
-    m.check_cell()
-  when "f"
-    m.set_flag()
-  when "x"
-    m.xray_print()
-  when "p"
-    m.current_print()
+  when 'c'
+    g.game_check_cell
+    puts
+    Printer.new(g).current_print
+  when 'f'
+    g.game_set_flag
+    puts
+    Printer.new(g).current_print
+  when 'x'
+    Printer.new(g).xray_print
+  when 'p'
+    Printer.new(g).current_print
   end
-  # if(print_mode == "x")
-  #   xray_print()
-  # elsif (print_mode == "p")
-  #   current_print()
-  # end
-  break if print_mode == "e" || !m.still_playing?
-  print_mode = "foo"
+  puts
+  break if print_mode == 'e' || !g.still_playing?
 end
 
 puts
 
-puts "Game Over!"
-if m.victory?
-  puts "You win!"
+puts 'Game Over!'
+if g.victory?
+  puts 'You win!'
 else
-  puts "You Lose!"
+  puts 'You Lose!'
 end
 puts
-  m.xray_print()
+Printer.new(g).xray_print
